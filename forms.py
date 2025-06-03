dd082c82e4c6e38b622aafb228978831919c8ea7
caioribeiro086 <caionw11@gmail.com>
2025-06-02 21:37:01 -0300

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Cliente, Venda
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+
+class VendaForm(forms.ModelForm):
+    class Meta:
+        model = Venda
+        fields = '__all__'
+        widgets = {
+            'data_venda': forms.DateInput(attrs={'type': 'date'})
+        }
